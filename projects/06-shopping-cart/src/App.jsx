0279ba8f1954a9d68dd0bbb4b1aa309ee50ea92b
santiMercado0694 <santimercado06_94@hotmail.com>
2024-02-08 13:34:24 -0300

    cree un context para los filters

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -2,13 +2,11 @@ import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { FiltersContext } from './context/filters.jsx'
 
 function useFilters () {
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+ const {filters, setFilters } = useContext(FiltersContext)
 
   const filterProducts = (products) => {
     return products.filter(product => {
